37cb86a8b13cbf8f93b8e49441dd8dfc401a45e9
Harry J Beckwith <harryjbeckwith@gmail.com>
2016-05-04 16:05:12 +0100

    Accordion
    Content Accordion using basic jQuery, good for faq.

---
+++ index.php
@@ -0,0 +1,25 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+<div id='container'>
+  <h1>jQuery FAQ slider</h1>
+  <div class='title'>
+    <h3>FAQ Slider Using jQuery</h3></div>
+  <ul class='faq'>
+    <li class='q'><i class="fa fa-chevron-right" aria-hidden="true"></i>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit?</li>
+    <li class='a'>"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain"</li>
+
+    <li class='q'><i class="fa fa-chevron-right" aria-hidden="true"></i>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit?</li>
+    <li class='a'>"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain"</li>
+
+    <li class='q'><i class="fa fa-chevron-right" aria-hidden="true"></i>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit?</li>
+    <li class='a'>"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain"</li>
+
+    <li class='q'><i class="fa fa-chevron-right" aria-hidden="true"></i>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit?</li>
+    <li class='a'>"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain"</li>
+
+    <li class='q'><i class="fa fa-chevron-right" aria-hidden="true"></i>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit?</li>
+    <li class='a'>"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain"</li>
+  </ul>
+
+
+</div>
